0fd5531a3496ea04ec99b438a42d0200d44b8988
danigoldblat <d0504109212@gmail.com>
2025-10-26 15:43:41 +0200

    added create_card

--- utils.py
+++ utils.py
@@ -1,2 +1,24 @@
+optinal_suites =["H","C","D","S"]
 def create_card(rank:str, suite:str) -> dict:
+    ranks={1=1,
+           2=2,
+           3=3,
+           4=4,
+           5=5,
+           6=6,
+           7=7,
+           8=8,
+           9=9,
+           10=10,
+           J=11,
+           Q=12,
+           K=13,
+           A=14}
+    if(rank not in ranks and suite not in optinal_suites):
+        return None
     
+    return{
+        "rank":rank,
+        "suite":suite,
+        "value":ranks[rank]
+    }
